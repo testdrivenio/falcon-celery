b895eecfaf9b0abef5b69f7a6b8e28d4d9e53706
Michael Herman <hermanmu@gmail.com>
2018-03-11 14:07:26 -0600

    added celery

---
+++ project/app/tasks.py
@@ -0,0 +1,28 @@
+# project/app/tasks.py
+
+
+import os
+from time import sleep
+
+import celery
+
+
+CELERY_BROKER = os.environ.get('CELERY_BROKER')
+CELERY_BACKEND = os.environ.get('CELERY_BACKEND')
+
+app = celery.Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
+
+
+@app.task
+def fib(n):
+    sleep(2)  # simulate slow computation
+    if n < 0:
+        return []
+    elif n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
+    else:
+        results = fib(n - 1)
+        results.append(results[-1] + results[-2])
+        return results
