537293453dd7d918a364aaec03331ed008e2b5a2
Michael Herman <hermanmu@gmail.com>
2021-04-19 20:44:13 -0500

    update dependencies

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -4,6 +4,9 @@
 import json
 
 import falcon
+from celery.result import AsyncResult
+
+from app.tasks import fib
 
 
 class Ping(object):
@@ -11,6 +14,34 @@ class Ping(object):
         resp.status = falcon.HTTP_200
         resp.text = json.dumps('pong!')
 
+
+class CreateTask(object):
+
+    def on_post(self, req, resp):
+        raw_json = req.stream.read()
+        result = json.loads(raw_json)
+        task = fib.delay(int(result['number']))
+        resp.status = falcon.HTTP_200
+        result = {
+            'status': 'success',
+            'data': {
+                'task_id': task.id
+            }
+        }
+        resp.text = json.dumps(result)
+
+
+class CheckStatus(object):
+
+    def on_get(self, req, resp, task_id):
+        task_result = AsyncResult(task_id)
+        result = {'status': task_result.status, 'result': task_result.result}
+        resp.status = falcon.HTTP_200
+        resp.text = json.dumps(result)
+
+
 app = falcon.App()
 
 app.add_route('/ping', Ping())
+app.add_route('/create', CreateTask())
+app.add_route('/status/{task_id}', CheckStatus())
--- project/test.py
+++ project/test.py
@@ -1,10 +1,11 @@
 # project/test.py
 
 import unittest
+from unittest.mock import patch
 
 from falcon import testing
 
-from app import app
+from app import app, tasks
 
 
 class TestAppRoutes(testing.TestCase):
@@ -15,6 +16,27 @@ class TestAppRoutes(testing.TestCase):
     def test_get_message(self):
         result = self.simulate_get('/ping')
         self.assertEqual(result.json, 'pong!')
+
+
+class TestCeleryTasks(unittest.TestCase):
+
+    # def test_fib_task(self):
+    #     self.assertEqual(tasks.fib.run(-1), [])
+    #     self.assertEqual(tasks.fib.run(1), [0, 1])
+    #     self.assertEqual(tasks.fib.run(3), [0, 1, 1, 2])
+    #     self.assertEqual(tasks.fib.run(5), [0, 1, 1, 2, 3, 5])
+
+
+    @patch('app.tasks.fib')
+    def test_mock_fib_task(self, mock_fib):
+        mock_fib.run.return_value = []
+        self.assertEqual(tasks.fib.run(-1), [])
+        mock_fib.run.return_value = [0, 1]
+        self.assertEqual(tasks.fib.run(1), [0, 1])
+        mock_fib.run.return_value = [0, 1, 1, 2]
+        self.assertEqual(tasks.fib.run(3), [0, 1, 1, 2])
+        mock_fib.run.return_value = [0, 1, 1, 2, 3, 5]
+        self.assertEqual(tasks.fib.run(5), [0, 1, 1, 2, 3, 5])
 
 
 if __name__ == '__main__':
